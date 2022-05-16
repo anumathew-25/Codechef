105b7cb41c4d62142672f0ad79f852bf7a32077e
anumathew-25 <79563511+anumathew-25@users.noreply.github.com>
2022-05-16 20:38:59 +0530

    problem:SNAKPROC

---
+++ SNAKPROC.c
@@ -0,0 +1,57 @@
+#include <stdio.h>
+	int N;
+void solve(){
+
+	scanf ("%d",&N);
+	char S[N];
+	scanf ("%s",&S);
+	char P[500]="";
+	int j=0;
+	int isHead,isValid;
+	for(int i=0;i<N;i++){
+	    if(S[i] !='.'){
+	        P[j]=S[i];
+	        j++;
+	    }
+	}
+	if(P[0]=='\0'){
+	    printf("Valid\n");
+	    return;
+	}
+	isHead =1;
+	isValid=1;
+	for(int i=0;i<j;i++){
+	    if(isHead==1&&P[i]=='T'){
+	        isValid=0;
+	        break;
+	    }
+	    if(isHead==0 &&P[i]=='H'){
+	        isValid =0;
+	        break;
+	    }
+	    if(isHead==1){
+	        isHead=0;
+	    }
+	    else{
+	        isHead=1;
+	    }
+	}
+	if(isHead==0){
+	    isValid=0;
+}
+if(isValid==1){
+	printf("Valid\n");
+}
+else{
+    printf("Invalid\n");
+}
+}
+int main(void) {
+    int T;
+    scanf("%d",&T);
+    while(T!=0){
+        solve();
+        T--;
+    }
+	return 0;
+}
