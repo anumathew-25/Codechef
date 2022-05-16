1807f179669036e80065f738df2222058c3c99f3
anumathew-25 <79563511+anumathew-25@users.noreply.github.com>
2022-05-16 21:51:52 +0530

    Add files via upload

---
+++ CATSDOGS.c
@@ -0,0 +1,32 @@
+#include <stdio.h>
+void solve(){
+    int cats,dogs,legs;
+    scanf("%d",&cats);
+    scanf("%d",&dogs);
+    scanf("%d",&legs);
+    if(legs%4 !=0){
+        printf("no\n");
+        return;
+    }
+    int totalAnimals=legs/4;
+    if(totalAnimals-dogs<0){
+        printf("no\n");
+        return;
+    }
+    int catsGround=totalAnimals-dogs;
+    int catsDogs=cats-catsGround;
+    if(catsDogs<0||catsDogs>2*dogs){
+        printf("no\n");
+        return;
+    }
+    printf("yes\n");
+}
+int main(void) {
+    int t=1;
+    scanf("%d",&t);
+    while(t--){
+        solve();        
+    }
+	return 0;
+}
+
